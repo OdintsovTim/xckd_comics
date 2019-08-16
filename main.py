78cb64e398f25e7fa549e46da1c56742718c3150
Odintsov Timur <timodin@yandex.ru>
2019-08-16 14:47:31 +0300

    added argparse

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
+import argparse
 import os
 import random
 
-from dotenv import load_dotenv
-import requests
 
 from load_image import get_image, get_image_url_filename_comment, get_last_comics_num
 from upload_image_to_vk import get_upload_server, post_photo, upload_photo_to_album, upload_photo_to_server
 
 
-load_dotenv()
-TOKEN = os.getenv('ACCESS_TOKEN')
-GROUP_ID = 185444788
-
-
 def main():
+    parser = argparse.ArgumentParser(description='This program uploads comics to group in VK')
+    parser.add_argument('token')
+    parser.add_argument('group_id')
+    args = parser.parse_args()
+    token = args.token
+
+    group_id = int(args.group_id)
     last_comics_num = get_last_comics_num()
     random_comics_num = random.randint(1, last_comics_num)
     img_url, img_name, img_comment = get_image_url_filename_comment(random_comics_num)
     get_image(img_url, img_name)
-    server_url = get_upload_server(GROUP_ID, TOKEN)
+    server_url = get_upload_server(group_id, token)
     server, photo, hash_vk =  upload_photo_to_server(server_url, img_name)
-    media_id, owner_id = upload_photo_to_album(server, photo, hash_vk, GROUP_ID, TOKEN)
-    post_photo(TOKEN, img_comment, GROUP_ID, media_id, owner_id)
+    media_id, owner_id = upload_photo_to_album(server, photo, hash_vk, group_id, token)
+    post_photo(token, img_comment, group_id, media_id, owner_id)
     os.remove(img_name)
 
 
